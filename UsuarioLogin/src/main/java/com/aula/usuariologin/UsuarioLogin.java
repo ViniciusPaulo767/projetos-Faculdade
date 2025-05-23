4f92f2fe0520de3bcc9220574df1e6532fc5c21a
Vinicius Paulo <viniciusaop767@gmail.com>
2025-05-23 14:41:54 -0300

    Adicionado teste de conexão

--- UsuarioLogin/src/main/java/com/aula/usuariologin/UsuarioLogin.java
+++ UsuarioLogin/src/main/java/com/aula/usuariologin/UsuarioLogin.java
@@ -1,7 +1,16 @@
 package com.aula.usuariologin;
+
+import java.sql.Connection;
+
 public class UsuarioLogin {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+        Connection conn = ConexaoBD.obterConexao();
+        System.out.println("Conexão bem-sucedida!");
+        conn.close();
+        } catch (Exception e) {
+        System.err.println("Erro na conexão: " + e.getMessage());
+        }
     }
 }
