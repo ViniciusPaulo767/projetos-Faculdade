38786767acd77abb249079e9eaddc9334fdd4008
Vinicius Paulo <viniciusaop767@gmail.com>
2025-05-23 13:22:36 -0300

    Primeiro commit

---
+++ UsuarioLogin/src/main/java/com/aula/usuariologin/ConexaoBD.java
@@ -0,0 +1,21 @@
+package com.aula.usuariologin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class ConexaoBD {
+    private static String host="localhost";
+    private static String porta="3306";
+    private static String db="db_usuarios";
+    private static String usuario="host";
+    private static String senha="@Vipp0703";
+    
+    public static Connection obterConexao() throws Exception{
+        String url=String.format(
+                "jdbc:mysql://%s:%s/%s",
+                host,
+                porta,
+                db
+        );
+        return DriverManager.getConnection(url,usuario,senha);
+    } 
+}
---
+++ UsuarioLogin/src/main/java/com/aula/usuariologin/UsuarioLogin.java
@@ -0,0 +1,7 @@
+package com.aula.usuariologin;
+public class UsuarioLogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
